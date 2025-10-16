d2e225a4ce3e6b947082c83657a54e78555eeb23
burstsort <burst149@gmail.com>
2025-10-16 15:15:22 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const images = document.getElementsByTagName('img');
+for(let i = 0; i < images.length; i++) {
+    if (images[i].onclick)
+        images[i].classList.add("hover")
+}
+
 function zoom(self) {
+    var opacity = 0
     var viewer = document.getElementById("viewer");
     var content = document.getElementById("content");
     var path = typeof self === 'string' ? self : self.src;
@@ -11,11 +18,33 @@ function zoom(self) {
         document.body.style.position = "fixed"
 
         viewer.innerHTML  = "<div class=\"image\"><img src=" + path + "></div>";
+        viewer.style.opacity = 0;
         viewer.style.display = "block";
+        var fadeIn = setInterval(function() {
+            if (opacity <= 1.0) {
+                viewer.style.opacity = opacity;
+                opacity += 0.2;
+            }
+            else {
+                viewer.style.opacity = 1;
+                clearInterval(fadeIn);
+            }
+        }, 10);
     }
 
     viewer.onclick = function() {
-        viewer.style.display = "none"
+        
+        var fadeOut = setInterval(function() {
+            if (opacity >= 0.0) {
+                viewer.style.opacity = opacity;
+                opacity -= 0.2;
+            }
+            else {
+                viewer.style.opacity = 0;
+                clearInterval(fadeOut);
+                viewer.style.display = "none"
+            }
+        }, 10);
 
         const scrollY = document.body.style.top
         document.body.style.position = ""
@@ -26,14 +55,15 @@ function zoom(self) {
 
 function expand(self) {
     var more = self.parentElement.parentElement.querySelector('.more');
-    if (more.style.display == "block"){
-        more.style.display = "none"
+    var thumbs = self.parentElement.parentElement.querySelector('.thumbnails');
+   more.style.transitionDuration = parseFloat(thumbs.offsetHeight) / 500 + "s";
+    if (more.offsetHeight > 0){
+        more.style.maxHeight = "0px"
         self.querySelector('.arrow').classList.add("down");
         self.querySelector('.arrow').classList.remove("up");
     } else {
-        more.style.display = "block"
+        more.style.maxHeight = parseFloat(thumbs.offsetHeight) + "px";
         self.querySelector('.arrow').classList.add("up");
         self.querySelector('.arrow').classList.remove("down");
     }
-    console.log(more.style.display)
 }
